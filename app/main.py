cd3d2405a2988cf74d66a2a38aa805e089db82d9
Andre Marcio <andre.marcio.ti@gmail.com>
2026-05-22 19:21:17 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -26,14 +26,13 @@ class Group:
     course: int
     students: list
 
-
-def write_groups_information(groups=None) -> Group:
+def write_groups_information(groups: list[Group] | None = None) -> int:
     with open("groups.pickle", "wb") as f:
         pickle.dump(groups, f)
         return max(len(group.students) for group in groups)
 
 
-def write_students_information(students=None) -> Student:
+def write_students_information(students: list[Student] | None = None) -> int:
     with open("students.pickle", "wb") as f:
         pickle.dump(students, f)
         return len(students)
